556ad71f1641cb2aadce436622da2cbc800d22b3
pyaesonemr02-arch <pyaesone.mr02@gmail.com>
2026-02-16 21:04:17 +0630

    Delete valentine-final directory

--- valentine-final/romantic-rennie/js/cinematic.js
+++
@@ -1,7 +0,0 @@
-/* =========================================
-   CINEMATIC INTRO CONTROL
-========================================= */
-
-document.addEventListener("DOMContentLoaded",()=>{
-    cinematicIntro.classList.add("hidden");
-});
